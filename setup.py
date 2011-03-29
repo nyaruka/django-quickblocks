2a3c41db247ec068a62a48b617c05efb0507cd51
Eugene Rwagasore <eugene@nyaruka.com>
2011-03-29 16:34:09 +0200

    Adopted the app to install using pip

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-quickblocks',
+    version = __import__('django_quickblock').__version__,
+    licence = 'BSD'
+
+    install_requires = [
+        'Django==1.3',
+        'PIL==1.1.7',
+        'django-reversion==1.3.3'
+        ],
+    dependency_links = [],
+    description = "",
+    long_description = open('README.org').read(),
+    author = 'Nicolas Pottier'
+    author_email = 'code@nyaruka.com',
+    url = 'http://www.nyaruka.com/#open',
+    download_url = 'http://github.com/nyaruka/django-quickblocks',
+
+    include_package_data = True,
+    packages = find_packages(),
+    zip_safe = False,
+    
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+    )
